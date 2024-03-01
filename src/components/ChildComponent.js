39af54460c2b16b12f86423cc313866dfd90a6fc
MinzPuiz <minhbui152@gmail.com>
2024-03-01 10:46:29 +0700

    fifth commit

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,25 +1,47 @@
 import React from "react";
 
-class ChildComponent extends React.Component {
-    render() {
-        console.log(">>> checking properties", this.props)
-        let { name, age, address, JobsTitle } = this.props;
-        return (
-            <>
-                <div className="job-lists">
-                    {
-                        JobsTitle.map((item, index) => {
-                            return(
-                                <div key={item.id}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </>
-        )
-    }
+// class ChildComponent extends React.Component {
+//     render() {
+//         console.log(">>> checking properties", this.props)
+//         let { name, age, address, JobsTitle } = this.props;
+//         return (
+//             <>
+//                 <div className="job-lists">
+//                     {
+//                         JobsTitle.map((item, index) => {
+//                             return(
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+const ChildComponent = (props) => {
+    //console.log('>>> Check Child props', props)
+
+    let { JobsTitle } = props;
+
+    return(
+        <>
+            <div className="job-lists">
+                {
+                    JobsTitle.map((item, index) => {
+                        return(
+                            <div key={item.id}>
+                                {item.title} - {item.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
 }
 
 export default ChildComponent;
